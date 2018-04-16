216585ef43c5bce2ea2fcd61d2a5446cce185e06
Eric <eluce@huntington.edu>
2018-04-16 11:29:46 -0700

    generalized file
    added readme, example file, and added more changes to the helper file to include more methods

---
+++ example.py
@@ -0,0 +1,47 @@
+
+import fmAPI
+
+fm = fmAPI.DataAPIv1('filemaker.company.com')
+
+fm.authenticate('solutionName', 'username', 'password')
+if fm.errorCode != 0:
+    print(fm.errorMessage)
+
+fm.get_records('layoutName')
+
+data = {
+    "fieldData": {
+        "First Name": "Joe",
+        "field2": "More text"
+    }
+}
+fm.create_record('layoutName', data)
+
+# you must get the record ID from a previous response from the FileMaker server.
+fm.delete_record('layoutName', 'recordId')
+
+data = {
+    "fieldData": {
+        "First Name": "Joe",
+        "field2": "More text"
+    }
+}
+fm.edit_record('layoutName', 'recordId', data)
+
+fm.get_record('layoutName', 'recordId')
+
+data = {
+ "query":[ {"Group": "=Surgeon", "Work State" : "CA"}, {"Group": "=Surgeon", "Work State" : "NY"}],
+ "sort":[ { "fieldName": "Work State", "sortOrder": "ascend" }, { "fieldName": "FirstName", "sortOrder": "ascend" } ]
+}
+fm.find_records('layoutName', data)
+
+
+data = { "globalFields": {
+     "tableName1::globalFieldName1":"globalFieldValue1",
+     "tableName2::globalFieldName2":"globalFieldValue2"
+     }
+}
+fm.set_globals(data)
+
+fm.logout('solutionName')
